59ad16836f81de493c48daada3828f2cf789d9b9
Chinna Eranna <meranna@cisco.com>
2020-02-28 12:27:37 +0530

    moved  templates back  to agent

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
-mkdir -p templates
-cp ./client/dist/index.html templates/
-cp ./client/dist/bootstrap.min.css templates/
-./bin/packr -v
+mkdir -p src/agent/templates
+cp ./client/dist/index.html src/agent/templates/
+cp ./client/dist/bootstrap.min.css src/agent/templates/
+cd src/agent
+../../bin/packr -v
+cd -
 export GOPATH=/Users/meranna/git/c-logs
 env GOOS=linux GOARC=386 go build -v -o ./bin/clogs agent
