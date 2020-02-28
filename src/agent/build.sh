25ada15d17160c8e387b334ce278ce89bd8a08ef
Chinna Eranna <meranna@cisco.com>
2020-02-28 11:52:57 +0530

    updated build script

--- src/agent/build.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p templates
-cp ../../client/dist/index.html templates/
-cp ../../client/dist/bootstrap.min.css templates/
-../../bin/packr -v
-export GOPATH=/Users/meranna/git/c-logs
-env GOOS=linux GOARC=386 go build -v -o ../../bin/clogs
