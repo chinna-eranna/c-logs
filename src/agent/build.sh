4ee5f1a627d6754140163cf3aa51eddbc3aa35e8
Chinna Eranna <meranna@cisco.com>
2020-02-28 11:45:02 +0530

    updated build.sh to create templates directory

--- src/agent/build.sh
+++ src/agent/build.sh
@@ -1,5 +1,6 @@
+mkdir -p templates
 cp ../../client/dist/index.html templates/
 cp ../../client/dist/bootstrap.min.css templates/
 ../../bin/packr -v
-export GOPATH=/Users/meranna/git/v-logs
+export GOPATH=/Users/meranna/git/c-logs
 env GOOS=linux GOARC=386 go build -v -o ../../bin/clogs
